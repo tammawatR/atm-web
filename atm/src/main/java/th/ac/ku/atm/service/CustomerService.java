f9c9009d6c1198398dd96daccf3db0c87729598e
newt <tammawat.r@ku.th>
2020-09-27 21:20:13 +0700

    atm web with jabc connection

--- atm/src/main/java/th/ac/ku/atm/service/CustomerService.java
+++ atm/src/main/java/th/ac/ku/atm/service/CustomerService.java
@@ -1,7 +1,9 @@
 package th.ac.ku.atm.service;
 
 import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import th.ac.ku.atm.data.CustomerRepository;
 import th.ac.ku.atm.model.Customer;
 
 
@@ -11,46 +13,44 @@ import java.util.List;
 
 @Service
 public class CustomerService {
-    private List<Customer> customerList;
+    private CustomerRepository repository;
 
-    @PostConstruct
-    public void postConstruct() {
-        this.customerList = new ArrayList<>();
+    public CustomerService(CustomerRepository repository) {
+        this.repository = repository;
     }
 
     public void createCustomer(Customer customer) {
         String hashPin = hash(customer.getPin());
         customer.setPin(hashPin);
-        customerList.add(customer);
+        repository.save(customer);
+    }
+
+    public Customer findCustomer(int id) {
+        try {
+            return repository.findById(id);
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
 
     public List<Customer> getCustomers() {
-        return new ArrayList<>(this.customerList);
+        return repository.findAll();
     }
-    public Customer findCustomer(int id) {
-        for (Customer customer : customerList) {
-            if (customer.getId() == id)
-                return customer;
-        }
-        return null;
-    }
+
     public Customer checkPin(Customer inputCustomer) {
-        // 1. หา customer ที่มี id ตรงกับพารามิเตอร์
         Customer storedCustomer = findCustomer(inputCustomer.getId());
 
-        // 2. ถ้ามี id ตรง ให้เช็ค pin ว่าตรงกันไหม โดยใช้ฟังก์ชันเกี่ยวกับ hash
         if (storedCustomer != null) {
-            String hashPin = storedCustomer.getPin();
-
-            if (BCrypt.checkpw(inputCustomer.getPin(), hashPin))
+            String storedPin = storedCustomer.getPin();
+            if (BCrypt.checkpw(inputCustomer.getPin(), storedPin))
                 return storedCustomer;
         }
-        // 3. ถ้าไม่ตรง ต้องคืนค่า null
         return null;
     }
     private String hash(String pin) {
         String salt = BCrypt.gensalt(12);
         return BCrypt.hashpw(pin, salt);
     }
-
 }
+
+
